6d00e77e7cbade27163acd090c3bf2eea14c04b9
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-07 18:03:15 -0400

    Fixed errors

--- google/pages/search.js
+++ google/pages/search.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import SearchHeader from '../components/SearchHeader'
-import Response from '../Response'
 import { useRouter } from 'next/router'
 import SearchResults from '../components/SearchResults'
 
@@ -30,10 +29,9 @@ export default Search;
 
 /* Handles server-side rendering */
 export async function getServerSideProps(context) {
-    const useDummyData = false;
     const startIndex = context.query.start || "0";
 
-    const data = useDummyData ? Response : await fetch(
+    const data = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
     ).then(response => response.json())
 
