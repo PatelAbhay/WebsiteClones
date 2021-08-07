ade0ff554a79e7874d0f6ca4144039895d04e921
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-07 10:49:13 -0400

    Added the header for the front page

---
+++ google/components/Profile.js
@@ -0,0 +1,11 @@
+function Profile({ url }) {
+    return (
+        <img
+            loading='lazy'
+            src={url}
+            alt='profile-pic'
+            className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110' />
+    )
+}
+
+export default Profile;
