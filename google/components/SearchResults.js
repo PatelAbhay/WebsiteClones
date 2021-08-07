00052eebe7b094c7ddd707321556291f55c28852
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-07 16:00:03 -0400

    Styled the search results page and handled the server side rendering

--- google/components/SearchResults.js
+++ google/components/SearchResults.js
@@ -8,15 +8,19 @@ function SearchResults({ results }) {
 
             {results.items?.map((result) => (
                 <div key={result.link} className="max-w-xl mb-8">
-                    <div>
+                    <div className="group">
                         <a href={result.link} className="text-sm">
                             {result.formattedUrl}
                         </a>
+                        <a href={result.link}>
+                            <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result.title}</h2>
+                        </a>
                     </div>
 
-                    <p>{result.snippet}</p>
+                    <p className="line-clamp-2">{result.snippet}</p>
                 </div>
             ))}
+
         </div>
     )
 }
