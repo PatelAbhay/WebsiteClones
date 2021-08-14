fe5d4f4e07ebff2d0aa2d2ec79e82649a04d98e2
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-14 10:14:42 -0400

    Styled the center of the header

--- facebook/components/Header.js
+++ facebook/components/Header.js
@@ -4,7 +4,7 @@ import CenterIcon from './CenterIcon'
 
 function Header() {
     return (
-        <header>
+        <header className="sticky top-o z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             {/* Search */}
             <div className="flex items-center">
                 <Image src="/facebook-logo.svg"
@@ -13,13 +13,14 @@ function Header() {
 
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 px-4 py-2">
                     <SearchIcon className="h-5 text-gray-500" />
-                    <input placeholder="Search" type="text" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"></input>
+                    <input placeholder="Search" type="text" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500
+                    flex-shrink"></input>
                 </div>
             </div>
 
             {/* Pages Icons */}
             <div className="flex items-center flex-grow">
-                <div>
+                <div className="flex space-x-6 md:space-x-2">
                     <CenterIcon Icon={HomeIcon} />
                     <CenterIcon Icon={FlagIcon} />
                     <CenterIcon Icon={PlayIcon} />
