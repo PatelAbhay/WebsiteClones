3ec15e6001a218ad50dd7350e47a68cd546c36d1
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-14 09:27:47 -0400

    Created the Header component

--- facebook/pages/index.js
+++ facebook/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -9,7 +10,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Page</h1>
+      <Header />
+
+      <main>
+        <h1>Sidebar</h1>
+        <h1>Posts</h1>
+        <h1>Contacts</h1>
+      </main>
     </div>
   )
 }
