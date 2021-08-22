bd764e0581dde92da8539cf80c41d270bb1f4c76
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-22 09:09:19 -0400

    Added firebase config

---
+++ slack/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVy3TGh5Q55bg5-6ItVur5CpDFl8F0K-E",
+    authDomain: "slackclone-eb4e8.firebaseapp.com",
+    projectId: "slackclone-eb4e8",
+    storageBucket: "slackclone-eb4e8.appspot.com",
+    messagingSenderId: "435954608623",
+    appId: "1:435954608623:web:3e1a5990f520924ef66e1a"
+};
