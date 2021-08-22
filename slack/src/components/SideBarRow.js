82f0f88cc21c866e0f3ff7044d0374b429574070
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-22 12:21:28 -0400

    Styled SideBarRow

--- slack/src/components/SideBarRow.js
+++ slack/src/components/SideBarRow.js
@@ -2,7 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SideBarRowContainer = styled.div`
+    display:flex;
+    font-size:12px;
+    align-items:center;
+    padding-left: 2px;
+    cursor:pointer;
 
+    :hover {
+        opacity: 0.9;
+        background-color: #340e36;
+    }
+
+    > h3 {
+        font-weight: 500;
+    }
+
+    > h3 > span {
+        padding: 15px;
+    }
 `;
 
 const OptionChannel = styled.div`
