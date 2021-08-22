a2d0e9a97661faa48d592a7e6db097e31bf034f0
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-22 09:12:18 -0400

    Added react-router setup

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <h1>Actual App</h1>
+
+      <Router>
+        <>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <h1>Main Home Page</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
