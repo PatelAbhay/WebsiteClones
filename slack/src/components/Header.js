aeef763329685055aebb1c81625a5ffa9066ee2b
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-22 10:24:21 -0400

    Styled Header component

--- slack/src/components/Header.js
+++ slack/src/components/Header.js
@@ -2,10 +2,18 @@ import styled from 'styled-components'
 import React from 'react'
 import { Avatar } from '@material-ui/core'
 import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 
 const HeaderContainer = styled.div`
     display: flex;
-
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0px;
+    background-color: var(--slack-color);
+    color: white;
 `;
 
 const HeaderLeft = styled.div`
@@ -28,6 +36,38 @@ const HeaderAvatar = styled(Avatar)`
     }
 `;
 
+const HeaderSearch = styled.div`
+    flex:0.4;
+    opacity: 1;
+    border-radius: 6px;
+    text-align: center;
+    background: hsl(297, 37%, 19%);
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    >input {
+        background-color: transparent !important;
+        border: none !important;
+        text-align: center;
+        min-width: 30vw;
+        outline: none;
+        color: white;
+    }
+`;
+
+const HeaderRight = styled.div`
+    flex:0.3;
+    display: flex;
+    align-items: flex-end;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 20px;
+    }
+`;
+
 function Header() {
     return (
         <HeaderContainer>
@@ -37,8 +77,16 @@ function Header() {
                 <AccessTimeIcon />
             </HeaderLeft>
 
-            {/* Center */}
+            {/* Center/Search */}
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder="Search"></input>
+            </HeaderSearch>
+
             {/* Right */}
+            <HeaderRight>
+                <HelpOutlineIcon />
+            </HeaderRight>
         </HeaderContainer>
     )
 }
