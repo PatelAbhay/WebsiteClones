9b14afa2e2460fa1f24e342965d0748029811026
Abhay Patel <patel.abhay1@hotmail.com>
2021-08-23 12:37:00 -0400

    Added the options the header

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -21,9 +21,13 @@ function Header() {
             </div>
 
             {/* Options */}
-            <div className="flex space-x-4">
-                <p>Become a Host</p>
-                <GlobeAltIcon className="h-6" />
+            <div className="flex space-x-4 items-center justify-end text-gray-400">
+                <p className="hidden md:inline cursor-pointer">Become a Host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+                    <MenuIcon className="h-6" />
+                    <UsersIcon className="h-6" />
+                </div>
             </div>
         </header>
     )
